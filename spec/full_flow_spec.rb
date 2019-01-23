974bfdb85ea6493cfcf7fbdfa95d4b13f35804e2
Shaun Keenan <skeenan@gmail.com>
2019-01-22 17:33:16 -0800

    Split tests a bit more p2

--- spec/full_flow_spec.rb
+++ spec/full_flow_spec.rb
@@ -26,24 +26,4 @@ describe "FullFlow" do
     @browser.goto "http://saucedemo.headless.saucelabs.com/cart.html"
     expect(@browser.divs(class: 'inventory_item_name').size).to eq 1
   end
-
-  it 'does stuff' do
-    @browser.goto "http://saucedemo.headless.saucelabs.com/inventory.html"
-    @browser.button(class: 'add-to-cart-button').click
-    expect(@browser.span(class: 'shopping_cart_badge').text).to eq '1'
-    @browser.goto "http://saucedemo.headless.saucelabs.com/cart.html"
-    expect(@browser.divs(class: 'inventory_item_name').size).to eq 1
-  end
-
-  it 'does more' do
-    @browser.goto "http://saucedemo.headless.saucelabs.com/inventory.html"
-    @browser.button(class: 'add-to-cart-button').click
-    @browser.button(class: 'add-to-cart-button').click
-    @browser.button(class: 'add-to-cart-button').click
-
-    expect(@browser.span(class: 'shopping_cart_badge').text).to eq '3'
-    @browser.goto "http://saucedemo.headless.saucelabs.com/cart.html"
-    expect(@browser.divs(class: 'inventory_item_name').size).to eq 3
-  end
-  
 end
