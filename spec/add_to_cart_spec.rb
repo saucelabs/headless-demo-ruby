d9a5cad60eb9630a8605e56c24f1b5cc6354701e
Shaun Keenan <skeenan@gmail.com>
2019-01-22 16:41:06 -0800

    oops

--- spec/add_to_cart_spec.rb
+++ spec/add_to_cart_spec.rb
@@ -3,12 +3,14 @@ require "spec_helper"
 describe "Cart" do
   before { @browser.goto 'www.saucedemo.com/inventory.html'}
 
-  it "adds one" do
+  it "adds three" do
+    @browser.button(class: 'add-to-cart-button').click
+    @browser.button(class: 'add-to-cart-button').click
     @browser.button(class: 'add-to-cart-button').click
 
-    expect(@browser.span(class: 'shopping_cart_badge').text).to eq '1'
+    expect(@browser.span(class: 'shopping_cart_badge').text).to eq '3'
     @browser.goto "https://www.saucedemo.com/cart.html"
-    expect(@browser.divs(class: 'inventory_item_name').size).to eq 1
+    expect(@browser.divs(class: 'inventory_item_name').size).to eq 3
   end
 
   it "adds two" do
