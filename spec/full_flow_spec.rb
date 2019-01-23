9bb4ddfd2c4743d876e605afb0363a5228955408
Shaun Keenan <skeenan@gmail.com>
2019-01-22 16:57:21 -0800

    faster

--- spec/full_flow_spec.rb
+++ spec/full_flow_spec.rb
@@ -1,9 +1,8 @@
 require "spec_helper"
 
 describe "FullFlow" do
-  before { @browser.goto 'saucedemo.headless.saucelabs.com'}
-
   it "acts like a user" do
+    @browser.goto "http://saucedemo.headless.saucelabs.com"
     @browser.text_field(data_test: 'username').set 'locked_out_user'
     @browser.text_field(data_test: 'password').set 'secret_sauce'
     @browser.button(type: 'submit').click
@@ -35,7 +34,6 @@ describe "FullFlow" do
     @browser.goto "http://saucedemo.headless.saucelabs.com/cart.html"
     expect(@browser.divs(class: 'inventory_item_name').size).to eq 1
 
-    @browser.goto "http://saucedemo.headless.saucelabs.com/inventory.html"
     @browser.button(class: 'add-to-cart-button').click
     @browser.button(class: 'add-to-cart-button').click
 
