b455adcf79d9d2a36b7ef5e7ef06eb4bc9fbadba
Shaun Keenan <skeenan@gmail.com>
2019-01-22 17:00:47 -0800

    split a bit more

--- spec/full_flow_spec.rb
+++ spec/full_flow_spec.rb
@@ -33,7 +33,11 @@ describe "FullFlow" do
     expect(@browser.span(class: 'shopping_cart_badge').text).to eq '1'
     @browser.goto "http://saucedemo.headless.saucelabs.com/cart.html"
     expect(@browser.divs(class: 'inventory_item_name').size).to eq 1
+  end
 
+  it 'does more' do
+    @browser.goto "http://saucedemo.headless.saucelabs.com/inventory.html"
+    @browser.button(class: 'add-to-cart-button').click
     @browser.button(class: 'add-to-cart-button').click
     @browser.button(class: 'add-to-cart-button').click
 
