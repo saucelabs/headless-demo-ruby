3311574f164dc70274e5120f3023fff105dd4425
Shaun Keenan <skeenan@gmail.com>
2019-03-25 08:40:06 -0700

    switch to backup site

--- spec/remove_from_cart_spec.rb
+++ spec/remove_from_cart_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
 describe "Cart" do
-  before { @browser.goto 'www.saucedemo.com/inventory.html'}
+  before { @browser.goto 'saucedemo.headless.saucelabs.com/inventory.html'}
 
   it "removes one" do
     @browser.button(class: 'add-to-cart-button').click
@@ -10,7 +10,7 @@ describe "Cart" do
     @browser.button(class: 'remove-from-cart-button').click
     expect(@browser.span(class: 'shopping_cart_badge').text).to eq '1'
 
-    @browser.goto "https://www.saucedemo.com/cart.html"
+    @browser.goto "http://saucedemo.headless.saucelabs.com/cart.html"
     expect(@browser.divs(class: 'inventory_item_name').size).to eq 1
   end
 end
