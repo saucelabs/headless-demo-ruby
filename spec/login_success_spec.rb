929b5c10d2a51ee9e866b963e4c6e68f53b5b844
Shaun Keenan <skeenan@gmail.com>
2019-04-23 13:05:41 -0700

    switch to saucedemo bucket

--- spec/login_success_spec.rb
+++ spec/login_success_spec.rb
@@ -1,13 +1,13 @@
 require "spec_helper"
 
 describe "Authentication" do
-  before { @browser.goto 'www.saucedemo.com'}
+  before { @browser.goto 'saucedemo.headless.saucelabs.com'}
 
   it "successful" do
   	@browser.text_field(data_test: 'username').set 'standard_user'
     @browser.text_field(data_test: 'password').set 'secret_sauce'
     @browser.button(type: 'submit').click
 
-    expect(@browser.url).to eq 'https://www.saucedemo.com/inventory.html'
+    expect(@browser.url).to eq 'http://saucedemo.headless.saucelabs.com/inventory.html'
   end
 end
